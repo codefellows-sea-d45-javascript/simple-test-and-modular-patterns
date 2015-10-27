a5d9bbdde0acf05b68967ae857700273a36805e7
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-10-27 11:56:16 -0700

    completed assignment

---
+++ jordan_bundy/lib/greet.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var exports = module.exports = {};
+
+var greet = function greet(name) {
+  
+  // If the process.argv was used, it should be an object, needs to be processed
+  if(typeof name === "object") {
+    name = name[2];
+  };
+
+  return 'hello ' + name;
+};
+
+exports.greet = greet;
+
+// If process exists and has a valid argument, output result to screen
+if(process.argv[2]){
+  console.log(greet(process.argv));
+};
+
