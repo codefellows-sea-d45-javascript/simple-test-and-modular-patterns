924184b104905e52a6fb5f7f8780e1cdbca0472d
Jordan Bundy <jordan@thefoldagram.com>
2015-10-27 13:19:15 -0700

    Update greet.js

--- jordan_bundy/lib/greet.js
+++ jordan_bundy/lib/greet.js
@@ -4,6 +4,8 @@ var exports = module.exports = {};
 
 var greet = function greet(name) {
   
+  // Not pretty, but a quick and dirty way to use process.argv
+  var name = name || process.argv;
   // If the process.argv was used, it should be an object, needs to be processed
   if(typeof name === "object") {
     name = name[2];
