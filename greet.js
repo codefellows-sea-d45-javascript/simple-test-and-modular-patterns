675df634c5a90a8919b5f6013db18e597e82d418
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-10-26 22:43:15 -0700

    files added

--- greet.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
- module.exports = {
-	greetme: function (name) {
-		return "hello " + name;
-	}
-}
--- index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-var greeting = require('./greet');
-console.log(greeting.greetme("Iryna"));
