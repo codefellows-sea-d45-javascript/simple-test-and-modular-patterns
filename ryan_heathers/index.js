7699b4a41a9b4a422ce349e25ea2a82573384f86
Ryan Heathers <ryan@ryanheathers.com>
2015-10-27 12:07:42 -0700

    Add command line parameter passing to greet function)

--- ryan_heathers/index.js
+++ ryan_heathers/index.js
@@ -1,4 +1,10 @@
 'use strict';
 
 var greet = require(__dirname + '/lib/greeting').greet;
-console.log(greet());
+
+if (process.argv[2] !== -1) { // check if string arg exists at position 2
+  console.log(greet(process.argv[2]));
+}
+else {
+  console.log(greet());
+}
