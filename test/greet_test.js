9df0dd9e3a447fd6ed8a0a8387019ea9fd835a32
Will Kitchell <william.kitchell@gmail.com>
2015-10-27 11:25:22 -0700

    folder name issues

--- test/greet_test.js
+++ test/greet_test.js
@@ -2,7 +2,6 @@
 
 var expect = require('chai').expect;
 var greet = require(__dirname + '/../lib/greet'); //must go back a directory, go into the lib directory, run greet.js
-var name = process.argv[2];
 
 describe('the greet function', function(){
   it('should greet the user by name', function(){
