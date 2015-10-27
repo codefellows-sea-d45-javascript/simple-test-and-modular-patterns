a008cd94b4c407493fdeb7c7cc4fd875ce7aab2a
Mike Skalandunas <mskalandunas@gmail.com>
2015-10-26 20:14:25 -0700

    add explanation for command line greeting

--- mike_skalandunas/greet.js
+++ mike_skalandunas/greet.js
@@ -2,3 +2,18 @@
 
 var args = process.argv.slice(2);
 console.log('Hello ' + args.join(' ') + '!');
+
+// How it works
+
+// The variable used to access arguments passed through the command
+// line is 'process.argv'.  The .slice(2) method is called because
+// process.argv is an array, and the first 2 indexes are node and
+// the file path of the greet.js file.
+
+// Source: http://blog.modulus.io/nodejs-scripts
+
+// Below are the console.logs I used to learn a little more.
+
+// console.log(args);
+// console.log(process.argv);
+// console.log(process.argv[1].toString());
