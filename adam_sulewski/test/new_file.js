a06c56e87e021d8368a48239333337edf94bed50
adam-james <adam.j.sulewski@gmail.com>
2015-10-28 12:31:14 -0700

    Remove extra file new_file

--- adam_sulewski/test/new_file.js
+++
@@ -1,9 +0,0 @@
-var exec = require('child_process'). exec;
-
-var greeting = '';
-
-exec('node ' + __dirname + '/../index.js test',
-  function(error, stdout, stderr) {
-    greeting = stdout;
-    console.log(greeting);
-});
