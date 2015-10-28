5d85189159f1cc4341d89237ad4f6ac30c168255
adam-james <adam.j.sulewski@gmail.com>
2015-10-28 08:08:58 -0700

    Set up package.json and start gulpfile

---
+++ adam_sulewski/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+// two tasks
+// run mocha/chai test
+// jshint all files except package.json
+
+// bonus: watch for changes and run the above
+// bonus: jshint options into a jshintrc file
+
