21d848a28a9af079e54ef79ab6ee0e3d8724094a
DanLombardy <lombardy.dan@gmail.com>
2015-10-29 12:16:13 -0700

    add gulp tasking to mocha and jshint. also add jshint functionality

--- dan_lombardy/lib/greet.js
+++ dan_lombardy/lib/greet.js
@@ -1,5 +1,5 @@
 "use strict";
-exports=module.exports={};
-exports.greet = function(name){
+
+module.exports = function(name){
   return "hello " + name;
 };
--- dan_lombardy/test/test_greet.js
+++ dan_lombardy/test/test_greet.js
@@ -1,12 +1,11 @@
 "use strict";
 
 var expect = require('chai').expect;
-var greeter =require(__dirname + '/../lib/greet');
+var greet =require(__dirname + '/../lib/greet');
 
 describe('a function that greets to the console', function(){
-
   it('should return "hello" and value of name variable', function(){
     var test = "Dan";
-    expect(greeter.greet(test)).to.equal("hello Dan");
+    expect(greet(test)).to.equal("hello Dan");
   });
 });
