487fe727ba30877169b3160dd7cbc88376260dac
Mike Skalandunas <mskalandunas@gmail.com>
2015-10-28 19:15:51 -0700

    matching up with today's class work

--- mike_skalandunas/test/greet_test.js
+++ mike_skalandunas/test/greet_test.js
@@ -1,18 +1,38 @@
 'use strict';
 
 var expect = require('chai').expect;
-var greet = require(__dirname + '/../lib/greet');
+var greet = require(__dirname + '/../bin/greet');
 
-describe('a simple greeting function', function() {
-  it('should return hello <name>', function() {
-    expect(greet('Jim')).to.eql('hello Jim');
-  })
-})
+describe('greet bin', function() {
+  before(function() {
+    this.backup = process.argv;
+    process.argv = {};
+  });
 
-// Failing test
+  after(function() {
+    process.argv = this.backup;
+    debugger;
+  });
 
-// describe('a simple greeting function', function() {
-//   it('should return hello <name>', function() {
-//     expect(greet('Jim')).to.eql('DSIOFJSIDOF');
-//   })
-// })
+  it('should greet anonymous', function() {
+    debugger;
+    expect(greet()).to.eql('hello anonymous');
+  });
+
+  describe('with arguments', function() {
+    before(function() {
+      this.backup = process.argv;
+      process.argv = ['node', 'myfile', 'test name'];
+    });
+
+    after(function() {
+      process.argv = this.backup;
+      debugger;
+    });
+
+    it('should pull from process.argv', function() {
+      expect(greet()).to.eql('hello test name');
+    });
+  });
+});
+debugger;
