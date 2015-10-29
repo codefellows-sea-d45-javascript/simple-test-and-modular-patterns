d0c84e5e87473abe0e5be7d4e406ed708bff4bf6
Will Kitchell <william.kitchell@gmail.com>
2015-10-29 11:33:35 -0700

    it works\!

--- will-kitchell/lib/greet.js
+++ will-kitchell/lib/greet.js
@@ -2,7 +2,7 @@
 
 function greet(name) { //create a greet function with an undefined name parameter
   return 'hello ' + name;
-};
+}
 
 console.log(greet(process.argv[2])); //set the name parameter in the greet function equal to process.argv[2]
 module.exports = greet; //export the new greet value
