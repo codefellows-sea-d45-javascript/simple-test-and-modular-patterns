eab5a09cba56c2f275d6a5e302673d1077251867
rastringer <ra1stringer@gmail.com>
2015-10-29 11:28:32 -0700

    Adds jshint file, gulp tests

--- robin_stringer/gulpfile.js
+++ robin_stringer/gulpfile.js
@@ -1,7 +1,22 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 var appFiles = ['index.js', './lib/**/*.js', 'bin/**/*.js'];
 var testFiles = ['test/**/*.js'];
+
+gulp.task('mocha:test', function() {
+  return gulp.src(testFiles, appFiles)
+  .pipe(mocha({
+      node: true,
+      globals: {
+        describe: true,
+        it: true,
+        before: true,
+        after: true
+      }
+    }))
+    .pipe(mocha({reporter: 'nyan'}));
+});
 
 gulp.task('jshint:test', function() {
   return gulp.src(testFiles)
