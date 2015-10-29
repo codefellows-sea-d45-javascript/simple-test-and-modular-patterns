0ce482351015cc40f3baa4246d8b2112351eb614
Mike Skalandunas <mskalandunas@gmail.com>
2015-10-28 20:25:49 -0700

    add gulp watcher for any js changes in any sub directory

--- mike_skalandunas/gulpfile.js
+++ mike_skalandunas/gulpfile.js
@@ -2,8 +2,14 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 var appFiles = ['index.js', 'lib/**/*.js', 'bin/**/*.js'];
 var testFiles = ['./test/**/*.js'];
+var watcher = gulp.watch('./**/*.js', ['jshint:test']);
+
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+})
 
 gulp.task('jshint:test', function() {
   return gulp.src(testFiles)
@@ -27,6 +33,10 @@ gulp.task('jshint:app', function() {
     .pipe(jshint.reporter('default'));
 });
 
+gulp.task('default', function() {
+  return gulp.src('greet_test.js', {read: false})
+  .pipe(mocha({reporter: 'spec'}));
+});
+
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
 gulp.task('default', ['jshint']);
-gulp.task('mocha');
