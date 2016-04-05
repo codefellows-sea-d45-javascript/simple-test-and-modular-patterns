59f53fc27e5d5a9f0cb3c5ae228d61407743e06f
Isabella Organ <isabellaorgan@gmail.com>
2016-04-05 08:56:32 -0700

    more style updates

--- isabella_organ/gulpfile.js
+++ isabella_organ/gulpfile.js
@@ -5,24 +5,25 @@ var testFiles = ['./test/**/*.js'];
 
 gulp.task('jshint:test', function() {
   return gulp.src(testFiles)
-		.pipe(jshint({
-  node: true,
-  globals: {
-    describe: true,
-    it: true,
-    before: true,
-    after: true
-  }
-		}))
+  .pipe(jshint({
+    node: true,
+    globals: {
+      describe: true,
+      it: true,
+      before: true,
+      after: true
+    }
+  }))
 		.pipe(jshint.reporter('default'));
 });
 
 gulp.task('jshint:app', function() {
   return gulp.src(appFiles)
-	.pipe(jshint({
-  node: true
-	}))
-	.pipe(jshint.reporter('default'));
+  .pipe(jshint({
+    node: true
+  }))
+  .pipe(jshint.reporter('default'));
 });
+
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
 gulp.task('default', ['jshint']);
